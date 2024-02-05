fac0a8317efa1f41909c459364bd37da928663f1
barbosaivan <ibarbosao@ufpso.edu.co>
2024-02-05 16:40:20 -0500

    Add : Se genera proyecto con Android Studio.
    - Se incluyen los archivos generados automáticamente por Android Studio.
    - Se añaden las librerías de ViewModel, Coroutines, Retrofit, Room, dagger hilt.
    - Se crea la clase PolygonCraftApplication y se agrega al AndroidManifest.

---
+++ app/src/main/java/com/ivanbarbosa/polygoncraft/PolygonApplication.kt
@@ -0,0 +1,13 @@
+package com.ivanbarbosa.polygoncraft
+
+import android.app.Application
+
+
+/* 
+* Project: PolygonCraft
+* From: com.ivanbarbosa.polygoncraft
+* Create by Ivan Barbosa on 5/02/2024 at 4:21 p. m.
+* Linkedin: https://www.linkedin.com/in/ivanbarbosaortega/
+*/
+class PolygonApplication:Application() {
+}
